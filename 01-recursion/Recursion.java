55b5f308d4cda319dbdaedc8be52592487066be9
Vansh Saboo <vsaboo30@stuy.edu>
2022-02-07 14:44:24 -0500

    Added recursive reverse method

---
+++ 01-recursion/Recursion.java
@@ -0,0 +1,9 @@
+public class Recursion{
+  public static String reverse(String s){
+    if(s.length() == 1){return s;}
+    return s.substring(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
+  }
+  public static void main(String[] args){
+    System.out.println( reverse("Foo") );
+  }
+}
