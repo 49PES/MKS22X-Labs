34f85c22369c1bb4e836c021c4a9309c28e903ba
Vansh Saboo <vsaboo30@stuy.edu>
2022-02-07 14:47:26 -0500

    Added a newton square root method

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -3,7 +3,17 @@ public class Recursion{
     if(s.length() == 1){return s;}
     return s.substring(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
   }
+
+  public static double sqrt(double n){
+    return sqrt(n, (double) n / 2);
+  }
+
+  public static double sqrt(double n, double guess){
+    if( ( n / guess + guess) / 2 < 0.00001) return guess;
+    return sqrt(n, ( n / guess + guess) / 2);
+  }
   public static void main(String[] args){
     System.out.println( reverse("Foo") );
+    System.out.println(  );
   }
 }
