eba814ca36101b7c05ce816b1ff627f06414f355
Vansh <vsaboo30@stuy.edu>
2022-02-08 17:56:23 -0500

    Added a flimsy countNoDoubleLetterWords method, needs to be worked upon further

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -50,16 +50,35 @@ public static int fibIter(int n, int f1, int f2){
       return f2;
   return fibIter(n - 1, f1 + f2, f1);
 }
+
+public static long countNoDoubleLetterWords(int length,String word){
+  if(length == 0){return 0L;}
+  long sum = 0;
+  for(char letter = 'a' ; letter <= 'z'; letter++){
+    if(word.length() == 0 || word.charAt(word.length() - 1) != letter){
+      // System.out.println(word + letter);
+      sum += countNoDoubleLetterWords(length - 1, word + letter) + 1;
+    }
+  }
+  return sum;
+}
   public static void main(String[] args){
   //  int[] list = {8, 2, 3, 1, 7};
     System.out.println( reverse("Foo") );
     System.out.println( sqrt(100) );
   //  System.out.println( fact(5) );
   //  System.out.println( isEven(5) + ", " + isEven(6) );
-//    System.out.println( partialSum(list, 0, 6) );
-//    makeWords(3, "");
-for(int i = 0; i < 10; i++){
-  System.out.println(fibIter(i, 1, 0));
-}
+  //    System.out.println( partialSum(list, 0, 6) );
+  //    makeWords(3, "");
+  for(int i = 0; i < 10; i++){
+    System.out.println(fibIter(i, 1, 0));
+  }
+  System.out.println(countNoDoubleLetterWords(1, "")); // Should print out 26
+
+  System.out.println(countNoDoubleLetterWords(2, "")); // Should print out 26^2 - 26 = 650
+  // Above actually just prints 676 (26^2) ?
+
+  System.out.println(countNoDoubleLetterWords(3, "")); // Should print out 26^3 - (2 * 26 * 25) - (26) = 16224 ?
+  // Above actually prints out 16926  (26^3 - 26^2 + 26) ?
   }
 }
