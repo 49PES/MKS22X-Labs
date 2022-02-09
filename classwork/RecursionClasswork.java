3370155daa95af503a4bc35b95cb9722bce4920f
Vansh Saboo <vsaboo30@stuy.edu>
2022-02-09 14:25:58 -0500

    RecursionClasswork - Added groupSum6 method

--- classwork/RecursionClasswork.java
+++ classwork/RecursionClasswork.java
@@ -3,6 +3,12 @@ public class RecursionClasswork{
 		if(start >= nums.length){return (target == 0);}
 		return ( groupSum(start + 1, nums, target) || groupSum(start + 1, nums, target - nums[start]) );
 	}
+
+	public boolean groupSum6(int start, int[] nums, int target){
+		if(start >= nums.length){return (target == 0);}
+		if(nums[start] == 6){return groupSum6(start + 1, nums, target - nums[start]); } // 6 must be chosen
+		return ( groupSum6(start + 1, nums, target) || groupSum6(start + 1, nums, target - nums[start]) );
+	}
 	public static void main(String[] args){
 
 	}
