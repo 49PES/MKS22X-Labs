12483a19aef9a1c80c714064671cadfae7bcdff1
Vansh <vsaboo30@stuy.edu>
2022-02-09 21:27:09 -0500

    RecursionClasswork - Changed methods to static (just for testing ease) - Fixed groupSum5 method (glanced over the instructions earlier - code applied only if there was a 5 and the succeeding # was 1, but not the general case with 5

--- classwork/RecursionClasswork.java
+++ classwork/RecursionClasswork.java
@@ -1,33 +1,27 @@
 public class RecursionClasswork{
-	public boolean groupSum(int start, int[] nums, int target){
+	public static boolean groupSum(int start, int[] nums, int target){
 		if(start >= nums.length){return (target == 0);}
 		return ( groupSum(start + 1, nums, target) || groupSum(start + 1, nums, target - nums[start]) );
 	}
 
-	public boolean groupSum6(int start, int[] nums, int target){
+	public static boolean groupSum6(int start, int[] nums, int target){
 		if(start >= nums.length){return (target == 0);}
 		if(nums[start] == 6){return groupSum6(start + 1, nums, target - nums[start]); } // 6 must be chosen
 		return ( groupSum6(start + 1, nums, target) || groupSum6(start + 1, nums, target - nums[start]) );
 	}
 
-	public boolean groupSum5(int start, int[] nums, int target){
-		if(start >= nums.length){ return (target == 0);}
+	public static boolean groupSum5(int start, int[] nums, int target){
+		if(start >= nums.length){ System.out.println("Start: " + start + ", Target = " + target); return (target == 0);}
 		if(nums[start] % 5 == 0 && start + 1 < nums.length && nums[start + 1] == 1){
+			System.out.println("Start: " + start + ", Target = " + target);
 			return (groupSum5(start + 2, nums, target - nums[start]) || groupSum5(start + 2, nums, target));
 			// If a number is a 5, and the number immediately succeeding it is a 1, skip over the 1's index by incrementing start by 2
 		}
+		System.out.println("Start: " + start + ", Target = " + target);
 		return ( groupSum5(start + 1, nums, target) || groupSum5(start + 1, nums, target - nums[start]) );
 	}
 
-	public boolean groupNoAdj(int start, int[] nums, int target){
-		if(start >= nums.length) return (target == 0);
-		if(start == 0){
-			return (groupNoAdj(start + 1, nums, target) || groupNoAdj(start + 2, nums, target - nums[start]));
-		}
-		return(groupNoAdj(start + 2, nums, target - nums[start]) || groupNoAdj(start + 2, nums, target) );
-	}
-
-	public boolean groupSumClump(int start, int[] nums, int target) {
+	public static boolean groupSumClump(int start, int[] nums, int target) {
   if(start >= nums.length) {return (target == 0);}
 
   if(start + 1 < nums.length && nums[start] == nums[start + 1]){
@@ -40,28 +34,28 @@ public class RecursionClasswork{
   return (groupSumClump(start + 1, nums, target - nums[start] ) || groupSumClump(start + 1, nums, target  ));
 }
 
-public boolean groupNoAdj(int start, int[] nums, int target) {
+	public static boolean groupNoAdj(int start, int[] nums, int target) {
 	  if (start >= nums.length){
 	    return (target == 0);
 		}
 	  return (groupNoAdj(start + 1, nums, target)) || (groupNoAdj(start + 2, nums, target - nums[start]));
 	}
 
-	 public boolean splitArray(int[] nums){
+	public static boolean splitArray(int[] nums){
 	 	return splitArrayHelper(nums, 0, 0, 0);
 	}
 
-	public boolean splitArrayHelper(int[] nums, int index, int sumOne, int sumTwo){
+	public static boolean splitArrayHelper(int[] nums, int index, int sumOne, int sumTwo){
 		if(index >= nums.length){return (sumOne == sumTwo);}
 		return (splitArrayHelper(nums, index + 1, sumOne + nums[index], sumTwo ) || splitArrayHelper(nums, index + 1, sumOne, sumTwo  + nums[index]));
 		// Either an element is a member of sumOne, or it is a member of sumTwo, so increment sumOne and sumTwo accordingly
 	}
 
-	public boolean split53(int[] nums){
+	public static boolean split53(int[] nums){
 		return split53Helper(nums, 0, 0, 0);
 	}
 
-	public boolean split53Helper(int[] nums, int index, int sumOne, int sumTwo){
+	public static boolean split53Helper(int[] nums, int index, int sumOne, int sumTwo){
 		if(index >= nums.length) {return (sumOne == sumTwo);}
 
 		if(nums[index] % 5 == 0) {
@@ -77,17 +71,17 @@ public boolean groupNoAdj(int start, int[] nums, int target) {
 
 	}
 
-	public boolean splitOdd10(int[] nums) {
+	public static boolean splitOdd10(int[] nums) {
   return splitOdd10Helper(nums, 0, 0, 0);
 	}
 
-	public boolean splitOdd10Helper(int[] nums, int index, int oddSum, int tenSum){
+	public static boolean splitOdd10Helper(int[] nums, int index, int oddSum, int tenSum){
   if(index >= nums.length){return (oddSum % 2 == 1 && tenSum % 10 == 0);}
   return(splitOdd10Helper(nums, index + 1, oddSum + nums[index], tenSum) || splitOdd10Helper(nums, index + 1, oddSum, tenSum + nums[index])  );
 
 }
 
 	public static void main(String[] args){
-		System.out.println(groupSum5(0, [2, 5, 10, 4], 12));
+		System.out.println(groupSum5(0, new int[] {2, 5, 10, 4}, 12));
 	}
 }
