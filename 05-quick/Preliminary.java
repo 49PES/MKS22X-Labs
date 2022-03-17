d3e1605d29eb464fe573ce539db47aaf823206d1
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2022-03-17 00:22:08 -0400

    Update Preliminary.java

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,76 +1,15 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-
 public class Preliminary{
-  public static int[] partition ( int [] data, int start, int end){
-      int[] temp = new int[end - start + 1];
-      int partition = data[(start + end) / 2];
-
-      int leftBound = start;
-      int rightBound = end;
-
-      boolean insertLeft = true;
-      for(int i = start; i <= end; i++){
-          if(i != (start + end) / 2){
-          if(data[i] < partition){
-            temp[leftBound] = data[i];
-            leftBound++;
-          }
-          else if(data[i] > partition){
-            temp[rightBound] = data[i];
-            rightBound--;
-          }
-          else if(data[i] == partition){
-            if(insertLeft){
-              temp[leftBound] = data[i];
-              leftBound++;
-            }
-            else{
-                temp[rightBound] = data[i];
-                rightBound--;
-            }
-              insertLeft = !insertLeft;
-            }
-          }
-
-      }
-
-      temp[leftBound] = partition;
-      for(int i = start; i <= end; i++){
-          data[i] = temp[i - start];
-      }
-      return data;
-
-  }
-
-    public static void swap(int[] data, int index1, int index2){
-        int temp = data[index1];
-        data[index1] = data[index2];
-        data[index2] = temp;
-    }
-
-	public static void main(String[] args) {
-		int[] bob = new int[] {7, 4, 2, 3, 1, 8, 6};
-		System.out.println( Arrays.toString(partition(bob, 0, 6)) );
-	}
-}
-
-/*
-import java.util.Arrays;
-
-public class Preliminary{
-  public static int[] partition (int[] data, int start, int end){
+  public static int partition (int[] data, int start, int end){
       // int[] temp = new int[end - start + 1];
       // int partition = data[(start + end) / 2];
-
       swap(data, start, (start + end) / 2);
       int pivot = data[start];
-
       int leftBound = start + 1;
       int rightBound = end;
-
       boolean insertLeft = true;
       while(leftBound != rightBound){
+
         if(data[leftBound] < pivot){
             leftBound++;
         }
@@ -85,20 +24,23 @@ public class Preliminary{
           else{
             swap(data, leftBound, rightBound);
           }
+          insertLeft = !insertLeft;
         }
       }
-
+      if(leftBound > 1 && data[leftBound - 1] < pivot){
+        swap(data, start, leftBound - 1);
+      }
+      return leftBound;
   }
-
     public static void swap(int[] data, int index1, int index2){
         int temp = data[index1];
         data[index1] = data[index2];
         data[index2] = temp;
     }
-
 	public static void main(String[] args) {
-		int[] bob = new int[] {7, 4, 2, 3, 1, 8, 6};
-		System.out.println( Arrays.toString(partition(bob, 0, 6)) );
+		int[] bob = new int[] {3, 4, 2, 3, 1, 8, 6};
+    // System.out.println(Arrays.toString(bob) );
+    System.out.println( partition(bob, 0, 6) );
+    // System.out.println(Arrays.toString(bob) );
 	}
 }
-*/
