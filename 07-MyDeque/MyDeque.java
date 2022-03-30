ab15c62b033fa60ebd9ce0caaef74ed4eb9b3960
Vansh Saboo <58910619+49PES@users.noreply.github.com>
2022-03-29 22:15:20 -0400

    Update MyDeque.java

--- 07-MyDeque/MyDeque.java
+++ 07-MyDeque/MyDeque.java
@@ -6,19 +6,19 @@ public class MyDeque<E>{
       public MyDeque(){
         @SuppressWarnings("unchecked")
         E[] d = (E[]) new Object[64];
-        this.data = d;
-        this.start = 32;
-        this.end = 33;
-        this.size = 0;
+        data = d;
+        start = 32;
+        end = 33;
+        size = 0;
       }
 
       public MyDeque(int initialCapacity){
         @SuppressWarnings("unchecked")
         E[] d = (E[]) new Object[initialCapacity];
-        this.data = d;
-        this.start = (initialCapacity + 1) / 2 ;
-        this.end = this.start + 1;
-        this.size = 0;
+        data = d;
+        start = (initialCapacity + 1) / 2 ;
+        end = start + 1;
+        size = 0;
 
         }
 
@@ -63,59 +63,20 @@ public class MyDeque<E>{
         return list + "]";
       }
 
-      public void addFirst(E element){
-        data[start] = element;
-        start--;
-        size++;
-        readjust();
-      }
-
-      public void addLast(E element){
-        data[end] = element;
-        end++;
-        size++;
-        readjust();
-      }
-
-      // public E removeFirst(){
-      //   start++;
-      //   readjust();
-      //   size--;
-      // }
-      // public E removeLast(){
-      //   end--;
-      //   readjust();
-      //   size--;
-      // }
-
-      public void resize(){
-        @SuppressWarnings("unchecked")
-        E[] d = (E[]) new Object[data.length * 2];
-        int newIndex = d.length / 4;
-        int index = start + 1;
-        while(true){
-          if(index == end){
-            break; // terminate
-          }
-
-          if(index == data.length){
-            index = 0; // Wrap around
-          }
-
-          d[newIndex] = data[index];
-          newIndex++;
-          index++;
+      public String toStringDebug(){
+        String list = "[" + data[0] ;
+        for(int i = 1; i < data.length; i++){
+          list += ", " + data[i];
         }
-
-        this.data = d;
+        return list + "]";
       }
-
       public E getFirst(){
         if(start + 1 == data.length)
           return data[0]; // Wrap around
         else
           return data[start + 1];
       }
+
       public E getLast(){
         if(end == 0)
           return data[data.length - 1]; // Wrap around
@@ -123,16 +84,71 @@ public class MyDeque<E>{
           return data[end - 1];
       }
 
-        public static void main(String[] args){
-            MyDeque<E> list = new MyDeque<E>(10);
-            System.out.println(list.start + " " + list.end);
-            list.addFirst(new String("foo") );
-            list.addFirst(new String("boo") );
-            list.addFirst(new String("moo") );
-            list.addLast(new String("too") );
-            System.out.println(list.toString() );
-            // System.out.println(list.getFirst() );
-            // System.out.println(list.start + " " + list.end);
+      public void addFirst(E element){
+        data[start] = element;
+        start--;
+        size++;
+        readjust();
+        resize();
+      }
+
+      public void addLast(E element){
+        data[end] = element;
+        end++;
+        size++;
+        readjust();
+        resize();
+      }
+
+
+
+      public E removeFirst(){
+        start++;
+        readjust();
+        size--;
+        if(start == 0){
+          return data[data.length - 1];
         }
+        return data[start - 1];
+
+      }
+      public E removeLast(){
+        end--;
+        readjust();
+        size--;
+        if(end == data.length - 1){
+          return data[0];
+        }
+        return data[end + 1];
+      }
+
+      public void resize(){
+        if(start == end){
+          @SuppressWarnings("unchecked")
+        E[] d = (E[]) new Object[data.length * 2];
+        int newIndex = d.length / 4;
+        int index = start;
+        while(true){
+          if(index == data.length){
+            index = 0; // Wrap around
+          }
+
+          d[newIndex] = data[index];
+          newIndex++;
+          index++;
+
+          if(index == end){
+            break; // terminate
+          }
+        }
+
+        data = d;
+        start = d.length / 4;
+        end = newIndex;
+
+      }
+      }
+
+
 
     }
