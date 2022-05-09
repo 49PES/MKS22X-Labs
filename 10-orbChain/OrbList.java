21206c25041855d2c52a28b9ace8cb99c33c14c7
Vansh Saboo <vsaboo30@stuy.edu>
2022-05-09 14:45:25 -0400

    NULL to null, a typo of sprintAttract() to springAttract()

---
+++ 10-orbChain/OrbList.java
@@ -0,0 +1,50 @@
+
+public class OrbList {
+  OrbNode first, last;
+
+  //create a fixed orb
+  OrbList() {
+    first = new FixedOrbNode(0, height/5);
+    last = new FixedOrbNode(width, height/5);
+    //link them to eachother
+    first.next = last;
+    last.prev = first;
+  }
+
+  /**
+  *complete this method
+  */
+  void add(OrbNode orb){
+    //insert orb at the end of the list before the last node.
+    last.prev.next = orb; // The link after the previous orb now points to this orb
+    orb.prev = last.prev; // Thing before now comes before the new orb
+
+    orb.next = last; // Connect the new orb to the end
+    last.prev = orb; // Connect the end to the new orb
+  }
+
+  /**
+  *complete this method
+  *process all nodes by running move.
+  */
+  void processAll() {
+    OrbNode current = first;
+    while(current != null){
+      current.move();
+      current = current.next;
+    }
+    //advance current to next until it is null, move() each of the nodes
+  }
+  /**
+  *complete this method
+  *Display all nodes by running their display().
+  */
+  void display() {
+    OrbNode current = first;
+    while(current != null){
+      current.display();
+      current = current.next;
+    }
+    //advance current to next until it is null, display() each of the nodes
+  }
+}
