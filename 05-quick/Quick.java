9036a28d4ecf4440f263ace34d107f3877cc61d2
Vansh <vsaboo30@stuy.edu>
2022-05-16 22:38:40 -0400

    Pushing repo up-to-date

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -1,30 +1,25 @@
 import java.util.Arrays;
 public class Quick{
 
-  // Helper method for partition - takes in index parameters + int array
-  public static void swap(int[] data, int index1, int index2){
-   int temp = data[index1];
-   data[index1] = data[index2];
-   data[index2] = temp;
-  }
-
-  public static int partition(int[] data, int start, int end){
-    // Swap a random element from start to end, inclusive, with the start element
-    swap(data, (int) ( Math.random() * (end - start + 1) ) + start, start);
-    int rightBound = end;
-    // Assign the value of the pivot for checking purposes
-    int pivot = data[start];
-    for(int i = start + 1; i <= rightBound; i++){
-      if(data[i] >= pivot){
-        swap(data, i, rightBound);
-        rightBound--;
-        i--; // If you swap, you want to keep the leftBound the same, so decrement to compensate for the increment
+  public static int partition(int[] data, int start, int end) {
+      int partition = data[end];
+      int leftBound = start - 1;
+      for (int index = start; index < end; index++) {
+        if (data[index] <= partition) {
+          leftBound++;
+          swap(data, leftBound, index);
+        }
       }
+      swap(data, leftBound + 1, end);
+      return leftBound + 1;
     }
-    swap(data, start, rightBound);
-    return rightBound;
-  }
 
+  // Helper method for partition
+  public static void swap(int[] data, int index1, int index2){
+    int temp = data[index1];
+    data[index1] = data[index2];
+    data[index2] = temp;
+  }
 
   public static int quickselect(int[] data, int k){
     int low = 0, high = data.length - 1;
@@ -36,49 +31,34 @@ public class Quick{
       else if(partitionIndex > k){
         partitionIndex = partition(data, low, partitionIndex - 1);
       }
-      else{break;}
+      else{break;} // If partitionIndex == k, then you have located the k'th smallest value
     }
     return data[partitionIndex];
   }
 
-  // Wrapper method
+
+  // WRAPPER METHOD
   public static void quicksort(int[] data){
     quicksort(data, 0, data.length - 1);
   }
 
-  // Helper method
-  public static void quicksort(int[] data, int lo, int hi){
-    if(lo >= hi) {return;}
-    int partition = partition(data, lo, hi);
-    quicksort(data, lo, partition - 1);
-    quicksort(data, partition + 1, hi);
+  // HELPER METHOD
+  public static void quicksort(int[] data, int start, int end) {
+    if (start >= end) return;
+    int partition = partition(data, start, end);
+    quicksort(data, start, partition - 1);
+    quicksort(data, partition + 1, end);
   }
 
-
-  // public static boolean check(int[] data){
-  //   for(int i = 0; i < data.length - 2; i++){
-  //     if(data[i] > data[i + 1]) return false;
-  //   }
-  //   return true;
-  // }
 
   public static void main(String[] args){
-    int[] bob = new int[1000000];
-    for(int i = 0; i < 1000000; i++){
-      bob[i] = (int) (Math.random() * 100000);
-    }
-
-    if(args.length > 0){
-      bob = new int[Integer.parseInt(args[0]) ];
-      for(int i = 0; i < Integer.parseInt(args[0]); i++){
-        bob[i] = (int) (Math.random() * 100000);
-      }
-    }
-    quicksort(bob);
-    // System.out.println(check(bob) );
-
-
+    // int[] bob = new int[15];
+    // for(int i = 0; i < 14; i++){bob[i] = (int) (Math.random() * 20); }
+    // System.out.println(Arrays.toString(bob) );
+    // quicksort(bob);
+    // System.out.println(Arrays.toString(bob) );
+    int[] bob = {14, 19, 18, 16, 9, 11, 0, 7, 14, 18};
+    partition(bob, 0, 9);
+    System.out.println(Arrays.toString(bob));
   }
-
-
 }
